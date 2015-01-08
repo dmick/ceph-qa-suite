322952641cfec18f704724f19e52efb51eddf379
David Zafman <dzafman@redhat.com>
2015-01-08 12:02:53 -0800

    Fix objectstore name changed to keyvaluestore
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- tasks/ceph_manager.py
+++ tasks/ceph_manager.py
@@ -184,7 +184,7 @@ class Thrasher:
                     self.ceph_manager.ctx.ceph.conf['osd']):
                 prefix = ("sudo ceph-objectstore-tool "
                           "--data-path {fpath} --journal-path {jpath} "
-                          "--type keyvaluestore-dev "
+                          "--type keyvaluestore "
                           "--log-file="
                           "/var/log/ceph/objectstore_tool.\\$pid.log ".
                           format(fpath=FSPATH, jpath=JPATH))
@@ -282,7 +282,7 @@ class Thrasher:
                     self.ceph_manager.ctx.ceph.conf['osd']):
                 prefix = ("sudo ceph-objectstore-tool "
                           "--data-path {fpath} --journal-path {jpath} "
-                          "--type keyvaluestore-dev "
+                          "--type keyvaluestore "
                           "--log-file="
                           "/var/log/ceph/objectstore_tool.\\$pid.log ".
                           format(fpath=FSPATH, jpath=JPATH))
